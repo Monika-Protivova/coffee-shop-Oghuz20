20521105bc3f819f2bf48d11b7cb581b62ebee6e
Oghuz Hasanli <oguzhsnli2@gmail.com>
2025-07-12 19:25:41 +0400

    OrderTable.kt

--- coffee-shop-application/src/main/kotlin/com/motycka/edu/order/OrderTable.kt
+++ coffee-shop-application/src/main/kotlin/com/motycka/edu/order/OrderTable.kt
@@ -1,3 +1,25 @@
 package com.motycka.edu.order
 
-// TODO implement OrderTable and OrderDAO
+import org.jetbrains.exposed.dao.LongEntity
+import org.jetbrains.exposed.dao.LongEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.LongIdTable
+
+object OrderTable : LongIdTable("orders") {
+    val customerId = long("customer_id")
+    val status = enumerationByName("status", 50, OrderStatus::class)
+}
+
+class OrderDAO(id: EntityID<Long>) : LongEntity(id) {
+    var customerId by OrderTable.customerId
+    var status by OrderTable.status
+
+    companion object : LongEntityClass<OrderDAO>(OrderTable)
+
+    fun toDTO() = OrderDTO(
+        id = id.value,
+        customerId = customerId,
+        status = status
+    )
+}
+
